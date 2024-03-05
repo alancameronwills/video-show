e0ca9bf9629e7191be0c12ddc64575e85dc01467
Alan Cameron Wills <alan@cameronwills.org>
2024-03-05 23:57:29 +0000

    add git

--- run.sh
+++ run.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 cd `dirname $BASH_SOURCE`
 
-# Start show after a delay, default 5s
+kill -9 `ps x | grep git | awk -- 'FNR == 1 {print $1}'`
+git pull &
 
-export DISPLAY=:0
-sleep ${1:-5}s
+# Start show after a delay, default 30s
+sleep ${1:-30}s
 
 kill -9 `ps x | grep vlc | awk -- 'FNR == 1 {print $1}'`
-
+export DISPLAY=:0
 vlc -I dummy --extraintf=http --http-password=vlc --fullscreen --loop --video-on-top \
  --no-osd --mouse-hide-timeout=1 --skip-frames \
   /media/alan/*/*.m* >/dev/null &
-date > ~/log.txt
 
 kill -9 `ps x | grep server | awk -- 'FNR == 1 {print $1}'`
-
 sudo node server.js 80 &
+
+date > ~/log.txt
