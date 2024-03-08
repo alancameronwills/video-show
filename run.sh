1cdd7f4c9827e30b46b0ef51eb90276cc0424598
Alan Cameron Wills <alan@cameronwills.org>
2024-03-08 02:51:55 +0000

    fix

--- run.sh
+++ run.sh
@@ -10,17 +10,17 @@ then
   # Start show after a delay, default 30s
   sleep ${1:-30}s
   
-  kill -9 `ps x | grep git | awk -- 'FNR == 1 {print $1}'`
+  kill -9 `ps x | grep git | awk '{print $1}'`
   git pull &
 
-  sleep 10s
+  sleep 20s
 fi
 
-kill -9 `ps x | grep vlc | awk -- 'FNR == 1 {print $1}'`
+kill -9 `ps x | grep vlc | awk '{print $1}'`
 export DISPLAY=:0
 vlc -I dummy --extraintf=http --http-password=vlc --fullscreen --loop --video-on-top \
  --no-osd --mouse-hide-timeout=1 --skip-frames \
   /media/alan/*/*.m* >/dev/null &
 
-kill -9 `ps x | grep server | awk -- 'FNR == 1 {print $1}'`
+sudo kill -9 `ps x | grep server | awk '{print $1}'`
 sudo node server.js 80 &
