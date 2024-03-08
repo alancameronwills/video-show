214449963e98048ab36bc987c1802061122f783b
Alan Cameron Wills <alan@cameronwills.org>
2024-03-08 03:21:39 +0000

    instant on

--- run.sh
+++ run.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 exec 2>&1 1>~/log.txt
-date
-
 cd `dirname $BASH_SOURCE`
-
-
-if [ "${1:-30}" -ne "0" ] 
-then
-  # Start show after a delay, default 30s
-  sleep ${1:-30}s
-  
-  kill -9 `ps x | grep git | awk '{print $1}'`
-  git pull &
-
-  sleep 20s
-fi
+date
 
 kill -9 `ps x | grep vlc | awk '{print $1}'`
 export DISPLAY=:0
@@ -24,3 +11,9 @@ vlc -I dummy --extraintf=http --http-password=vlc --fullscreen --loop --video-on
 
 sudo kill -9 `ps aux | grep server | awk '{print $2}'`
 sudo node server.js 80 &
+
+sleep 20s
+kill -9 `ps x | grep git | awk '{print $1}'`
+git pull &
+
+
