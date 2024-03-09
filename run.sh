9471f42c68c5c2434b25c8b4db51275edcf77edb
Alan Cameron Wills <alan@cameronwills.org>
2024-03-09 09:42:55 +0000

    add audio device selection

--- run.sh
+++ run.sh
@@ -13,7 +13,8 @@ sleep ${1:-30}s
 kill -9 `ps x | grep vlc | awk -- 'FNR == 1 {print $1}'`
 export DISPLAY=:0
 vlc -I dummy --extraintf=http --http-password=vlc --fullscreen --loop --video-on-top \
- --no-osd --mouse-hide-timeout=1 --skip-frames \
+  --no-osd --mouse-hide-timeout=1 --skip-frames \
+  --alsa-audio-device hw:0,0 \
   /media/alan/*/*.m* >/dev/null &
 
 kill -9 `ps x | grep server | awk -- 'FNR == 1 {print $1}'`
