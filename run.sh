ee81a09bad8fe152eb865febf27fc2b36d25ba89
Alan Cameron Wills <alan@cameronwills.org>
2024-03-10 11:22:10 +0000

    colours

--- run.sh
+++ run.sh
@@ -4,18 +4,17 @@ date
 
 cd `dirname $BASH_SOURCE`
 
-kill -9 `ps x | grep git | awk -- 'FNR == 1 {print $1}'`
+kill -9 `ps ux | grep git | awk -- '{print $2}'`
 git pull &
 
 # Start show after a delay, default 30s
 sleep ${1:-30}s
 
-kill -9 `ps x | grep vlc | awk -- 'FNR == 1 {print $1}'`
+kill -9 `ps ux | grep vlc | awk -- '{print $2}'`
 export DISPLAY=:0
 vlc -I dummy --extraintf=http --http-password=vlc --fullscreen --loop --video-on-top \
   --no-osd --mouse-hide-timeout=1 --skip-frames \
-  --alsa-audio-device hw:1,0 \
   /media/alan/*/*.m* >/dev/null &
 
-kill -9 `ps x | grep server | awk -- 'FNR == 1 {print $1}'`
+kill -9 `ps x | grep server | awk -- '{print $2}'`
 sudo node server.js 80 &
